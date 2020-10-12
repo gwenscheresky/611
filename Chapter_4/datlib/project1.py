b783388d84879a946ad5b21627dad1022cff719a
gwenscheresky <gwen.scheresky@ndsu.edu>
2020-10-11 23:55:02 -0500

    Add

--- Chapter_4/datlib/project1.py
+++ Chapter_4/datlib/project1.py
@@ -3,26 +3,13 @@ import numpy as np
 import matplotlib.pyplot
 
 
-data = pd.ExcelFile("C:\Users\gwens\OneDrive\Documents\611\Gwen_Project_Data.xlsx")
-names = data.sheet_names
+data = pd.ExcelFile("Gwen_Project_Data.xlsx")
+names = list(data.sheet_names)
 
 data_dct = {}
 for name in names:
-    data_dct[names] = data.parse(name)
+    data_dct[names] = data.parse(name, index_col = "Date")
     
 print(data_dct)
 
-#to check
-#for key in data_dct:
-    #val = data_dct[key]
-    #print(data_dct[key])
-   
-#for key, val in data_dct.items():
-    #print(val)
-    
-"""
-Created on Wed Oct  7 14:04:11 2020
-
-@author: gwens
-"""
 
